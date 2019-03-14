587c127cb1eee2418cea221a50da5158b0fd2031
Thor <thorhuga1@live.dk>
2019-03-14 10:40:37 +0100

    Updated repo with code
    Random Number Generator matrix benchmark, first itteration

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let genRandomNumbers n m =
+    let rnd = System.Random()
+    Array2D.init n m (fun _ _  -> rnd.Next())
+
+[<EntryPoint>]
+let main argv =
+    let l = genRandomNumbers 4 4
+    printfn "%A" l
+    0 // return an integer exit code
